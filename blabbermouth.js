49d85b99573e62272db0ff4f7e0fc14e1f7b42e9
Phil <103880143+Sharppegs@users.noreply.github.com>
2023-02-07 16:12:41 +0100

    Add files via upload

---
+++ blabbermouth.js
@@ -0,0 +1,18 @@
+import {cards} from './cardData.js'
+
+const menuCards = document.getElementById("menu-cards")
+
+const cardsDisplay = cards.map(item => 
+        `<div class="card-info">
+           
+            <img src=${item.image} alt=${item.id} />
+            <h5 class="item-title">${item.title}</h5>
+            
+        </div>`
+).join(' ')
+
+menuCards.innerHTML += cardsDisplay
+    
+
+
+console.log(cards)
---
+++ cardData.js
@@ -0,0 +1,47 @@
+const cards = [
+    {
+        id: 0,
+        title: "Card Number One",
+        image: "./img/blabber-small.jpg",
+        text: "jdsakdfkdfkfdkfdkjlfdjkfdskj",
+        link: "#"
+    },
+    {
+        id: 1,
+        title: "Card Number Two",
+        image: "./img/example-3.jpg",
+        text: "jdsakdfkdfkfdkfdkjlfdjkfdskj",
+        link: "#"
+    },
+    {
+        id: 2,
+        title: "Card Number Three",
+        image: "./img/example-2.jpg",
+        text: "jdsakdfkdfkfdkfdkjlfdjkfdskj",
+        link: "#"
+    },
+    {
+        id: 3,
+        title: "Card Number Four",
+        image: "./img/example-1.jpg",
+        text: "jdsakdfkdfkfdkfdkjlfdjkfdskj",
+        link: "#"
+    },
+    {
+        id: 4,
+        title: "Card Number One",
+        image: "./img/blabber-small.jpg",
+        text: "jdsakdfkdfkfdkfdkjlfdjkfdskj",
+        link: "#"
+    },
+    {
+        id: 5,
+        title: "Card Number Two",
+        image: "./img/example-3.jpg",
+        text: "jdsakdfkdfkfdkfdkjlfdjkfdskj",
+        link: "#"
+    },
+    
+]
+
+export {cards}
