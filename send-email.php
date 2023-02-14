316a8676c3588c8bdfc7b4d61451ba7bcdc43089
Phil <103880143+Sharppegs@users.noreply.github.com>
2023-02-14 16:34:05 +0100

    php contact form

---
+++ send-email.php
@@ -0,0 +1,33 @@
+<?php
+
+$name = $_POST("name");
+$email = $_POST("email");
+$time = $_POST("time");
+$message = $_POST("message");
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "devphilbolton@gmail.com";
+$mail->Password = "Uncledad2007";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("devphilbolton@gmail.com", "Phil");
+
+$mail->Subject = $time;
+$mail->Body = $message;
+
+$mail->send();
+
+echo "email sent";
