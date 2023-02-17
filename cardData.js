9a3f01b04dfcb3e3f2140c9063db301dc4e3f9d8
Phil <103880143+Sharppegs@users.noreply.github.com>
2023-02-17 17:43:41 +0100

    courses added

--- cardData.js
+++ cardData.js
@@ -1,14 +1,14 @@
 const cards = [
     {
         id: 0,
-        title: "Conversation classes",
+        title: "Conversation Classes",
         image: "./img/blabber-small.jpg",
         text: "jdsakdfkdfkfdkfdkjlfdjkfdskj",
         link: "conversation.html"
     },
     {
         id: 1,
-        title: "Private Classes",
+        title: "Private Tutoring",
         image: "./img/example-3.jpg",
         text: "jdsakdfkdfkfdkfdkjlfdjkfdskj",
         link: "private-classes.html"
@@ -18,7 +18,7 @@ const cards = [
         title: "Eight-Week Courses",
         image: "./img/example-2.jpg",
         text: "jdsakdfkdfkfdkfdkjlfdjkfdskj",
-        link: "#"
+        link: "courses.html"
     }
 
     
