c536bef0b35c505d4a611cf2c49ae3d4d723f614
Phil <103880143+Sharppegs@users.noreply.github.com>
2023-02-20 18:15:08 +0100

    course updated to be responsive
    potential youtube embedding too

---
+++ courseData.js
@@ -0,0 +1,34 @@
+const courses = [
+    {
+        id: 0,
+        level: "B1+",
+        name: "INTERMEDIATE 1",
+        weeks: "8 WEEKS (32 HOURS)",
+        times: "MONDAYS AND WEDNESDAYS 10:00 - 12:00",
+        start: "20/03/2023",
+        color: "b1"
+    },
+    {
+        id: 1,
+        level: "B2",
+        name: "INTERMEDIATE 2",
+        weeks: "8 WEEKS (32 HOURS)",
+        times: "MONDAYS AND WEDNESDAYS 14:00 - 16:00",
+        start: "20/03/2023",
+        color: "b2"
+    },
+    {
+        id: 2,
+        level: "B2+",
+        name: "INTERMEDIATE PLUS",
+        weeks: "8 WEEKS (32 HOURS)",
+        times: "TUESDAYS AND THURSDAYS 10:00 - 12:00",
+        start: "28/03/2023",
+        color: "b3"
+    }
+   
+
+    
+]
+
+export {courses}
---
+++ courses.js
@@ -0,0 +1,33 @@
+import {courses} from './courseData.js'
+
+
+const courseCards = document.getElementById("course-cards")
+
+const courseDisplay = courses.map(course => 
+        `<div class="course-card-info ${course.color}">
+            <div class=course-titles>
+                <h1 class="course-level">${course.level}</h1>
+            </div>
+            <h3 class="course-title">${course.name}</h3>
+            <div>
+                <h5 class="fw-bold">Course Length</h5>
+                <p>${course.weeks}</p>
+            </div>
+            <div>
+                <h5 class="fw-bold">When<h5>
+                <p>${course.times}</p>
+            </div>
+            <div>
+                <h5 class="fw-bold">Begins</h5>
+                <p>${course.start}</p>
+            </div>
+            
+        </div>`
+).join(' ')
+
+courseCards.innerHTML += courseDisplay
+
+
+
+
+console.log(courses)
