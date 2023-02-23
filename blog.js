8667b17b27d02bb4016e10d19155fee25ab2f431
Sharppegs <boltonjnr@gmail.com>
2023-02-23 17:52:16 +0100

    blog

--- blog.js
+++ blog.js
@@ -4,20 +4,22 @@ import {blogArticles} from './blogData.js'
 const blogs = document.getElementById("blogs")
 
 const blogsDisplay = blogArticles.map(article => 
-        `<div class="blog-card">
-            <div class="blog-card-body">
-            <img class="article-image" src=${article.image} />
-             <div class="blog-card-title">   
-            <h1>${article.title}</h1>
-            </div>
+        `<a href=${article.link}>
+            <div class="blog-card ${article.color}">
+                <div class="blog-card-body">
+                <img class="article-image" src=${article.image} />
+                <div class="blog-card-title">   
+                <h1>${article.title}</h1>
+                </div>
+                
             
-           
-            <p class="lead">${article.tagline}</p>
-               
-            <a href=${article.link} class="blog-link">Read More...</a>
-                      
+                <p class="lead">${article.tagline}</p>
+                
+                <button class="blog-link">Read More...</button>
+                        
+                </div>
             </div>
-        </div>`
+        </a>`
     
 ).join('')
 
--- blogData.js
+++ blogData.js
@@ -3,27 +3,30 @@ const blogArticles = [
     {
         id: 0,
         title: "Excuse me?",
-        image: "./img/pardon.jpg",
+        image: "./blogs/excuse-me/images/pardon.jpg",
         tagline: "How to politely ask someone to repeat information",
-        link: "#"
+        link: "./blogs/excuse-me/excuse-me.html",
+        color: "secondary-bg"
         
     },
-    {
-        id: 1,
-        title: "Excuse me?",
-        image: "./img/pardon.jpg",
-        tagline: "How to politely ask someone to repeat information",
-        link: "#"
+    // {
+    //     id: 1,
+    //     title: "Excuse me?",
+    //     image: "./img/pardon.jpg",
+    //     tagline: "How to politely ask someone to repeat information",
+    //     link: "#",
+    //     color: "primary-bg"
         
-    },
-    {
-        id: 2,
-        title: "Excuse me?",
-        image: "./img/pardon.jpg",
-        tagline: "How to politely ask someone to repeat information",
-        link: "#"
+    // },
+    // {
+    //     id: 2,
+    //     title: "Excuse me?",
+    //     image: "./img/pardon.jpg",
+    //     tagline: "How to politely ask someone to repeat information",
+    //     link: "#",
+    //     color: "secondary-bg"
         
-    }
+    // }
 
 ]
 
