3389abe473e0424f4c6e973d6a3420733dccb529
Sharppegs <boltonjnr@gmail.com>
2023-03-01 15:28:06 +0100

    convo blog

--- blog.js
+++ blog.js
@@ -24,3 +24,4 @@ const blogsDisplay = blogArticles.map(article =>
 ).join('')
 
 blogs.innerHTML += blogsDisplay
+
--- blogData.js
+++ blogData.js
@@ -9,25 +9,31 @@ const blogArticles = [
         color: "secondary-bg"
         
     },
-    // {
-    //     id: 1,
-    //     title: "Excuse me?",
-    //     image: "./img/pardon.jpg",
-    //     tagline: "How to politely ask someone to repeat information",
-    //     link: "#",
-    //     color: "primary-bg"
+    {
+        id: 1,
+        title: "Making Conversation",
+        image: "./blogs/making-conversation/images/agree.jpg",
+        tagline: "Top Tips for a Successful Conversation",
+        link: "./blogs/making-conversation/make-conversation.html",
+        color: "primary-bg"
         
-    // },
+    },
     // {
     //     id: 2,
-    //     title: "Excuse me?",
-    //     image: "./img/pardon.jpg",
-    //     tagline: "How to politely ask someone to repeat information",
+    //     title: "Coming soon...",
+    //     image: "",
+    //     tagline: "",
     //     link: "#",
     //     color: "secondary-bg"
         
     // }
 
 ]
+
+// function getPosts() {
+//     fetch("https://eu-west-2.cdn.hygraph.com/content/cleo2hy1i0r9201t8h227fuyw/master")
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+// }
 
 export {blogArticles}
